0d4791d699d7cae0401bc15c504277d004a12651
Vincent Tene <vincent.tene@vt-it-consulting.de>
2023-10-31 20:07:16 +0100

    new project added

---
+++ Mastering-blazor/src/blazor-webassembly/BooksStore/BooksStore/Services/BooksService.cs
@@ -0,0 +1,35 @@
+﻿using BooksStore.Models;
+
+namespace BooksStore.Services;
+
+public class BooksService : IBooksService
+{
+    static List<Book> _allBooks = new List<Book>
+    {
+        new()
+        {
+            AuthorName = "John Smith",
+            PublishingDate = new DateTime(2021, 01,
+                12),
+            Title = "Blazor WebAssembly Guide"
+        },
+        new()
+        {
+            AuthorName = "John Smith",
+            PublishingDate = new DateTime(2022, 03,
+                13),
+            Title = "Mastering Blazor WebAssembly",
+        },
+        new()
+        {
+            AuthorName = "John Smith",
+            PublishingDate = new DateTime(2022, 08, 13),
+            Title = "Learning Blazor from A to Z"
+        }
+    };
+
+    public Task<List<Book>> GetAllBooksAsync()
+    {
+        return Task.FromResult(_allBooks);
+    }
+}
---
+++ Mastering-blazor/src/blazor-webassembly/BooksStore/BooksStore/Services/ILoggingService.cs
@@ -0,0 +1,15 @@
+﻿namespace BooksStore.Services;
+
+public interface ILoggingService
+{
+   public void Log(string message);
+}
+
+
+public class ConsoleLoggingService : ILoggingService
+{
+   public void Log(string message)
+   {
+      Console.WriteLine(message);
+   }
+}
